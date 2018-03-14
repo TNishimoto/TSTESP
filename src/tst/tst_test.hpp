7bf46031e453a8fbc717cd4f8298463129c9e583
Nishimoto <a32b16c4@gmail.com>
2018-03-14 17:40:51 +0900

    gpl

--- src/tst/tst_test.hpp
+++ src/tst/tst_test.hpp
@@ -1,21 +1,20 @@
-/* 
- * Copyright (C) 2018, Takaaki Nishimoto, all rights reserved.
- *
- *   Redistribution and use in source and binary forms, with or without
- *   modification, are permitted provided that the following conditions
- *   are met:
- *
- *   1. Redistributions of source code must retain the above Copyright
- *      notice, this list of conditions and the following disclaimer.
- *
- *   2. Redistributions in binary form must reproduce the above Copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *
- *   3. Neither the name of the authors nor the names of its contributors
- *      may be used to endorse or promote products derived from this
- *      software without specific prior written permission.
- */
+/*
+* Copyright (c) 2018 Takaaki Nishimoto
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published bytes
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 #pragma once
 #include <iostream>
